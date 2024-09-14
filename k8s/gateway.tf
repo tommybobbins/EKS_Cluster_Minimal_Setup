a9d965863f6450f1ba5f6a55874d2f017c280068
Tim Gibbon <tng@chegwin.org>
2024-09-14 19:53:31 +0100

    Tofu fmt

--- k8s/gateway.tf
+++ k8s/gateway.tf
@@ -1,33 +1,33 @@
 resource "helm_release" "kong_gateway" {
-  count   = var.gateway_flavour == "kong" ? 1 : 0
-  name    = "kong-gateway"
+  count            = var.gateway_flavour == "kong" ? 1 : 0
+  name             = "kong-gateway"
   create_namespace = true
-  chart   = "./gateway/kong-gateway"
-  timeout = 600
+  chart            = "./gateway/kong-gateway"
+  timeout          = 600
 }
 
 resource "helm_release" "gatewayapi" {
-  count   = var.gateway_flavour == "nginx" ? 1 : 0
-  name    = "standard-gatewayapi"
+  count            = var.gateway_flavour == "nginx" ? 1 : 0
+  name             = "standard-gatewayapi"
   create_namespace = true
-  chart   = "./gateway/standard-gatewayapi"
-  timeout = 600
+  chart            = "./gateway/standard-gatewayapi"
+  timeout          = 600
 }
 
 resource "helm_release" "nginx_gateway_fabric" {
-  count   = var.gateway_flavour == "nginx" ? 1 : 0
- name    = "nginx-gateway-fabric"
-  namespace = "nginx-gateway"
+  count            = var.gateway_flavour == "nginx" ? 1 : 0
+  name             = "nginx-gateway-fabric"
+  namespace        = "nginx-gateway"
   create_namespace = true
-  chart   = "./gateway/nginx-gateway-fabric"
-  timeout = 600
+  chart            = "./gateway/nginx-gateway-fabric"
+  timeout          = 600
   values = [
-     "${templatefile("./gateway/nginx-gateway-fabric/values.yaml", {
-      cert_arn          = data.aws_acm_certificate.ssl_cert.arn
-      public_subnets    = join(",", sort(data.aws_subnets.public_subnets.ids))
-      })}"
+    "${templatefile("./gateway/nginx-gateway-fabric/values.yaml", {
+      cert_arn       = data.aws_acm_certificate.ssl_cert.arn
+      public_subnets = join(",", sort(data.aws_subnets.public_subnets.ids))
+    })}"
   ]
-  depends_on = [ helm_release.gatewayapi ]
+  depends_on = [helm_release.gatewayapi]
 }
 
 
@@ -56,31 +56,31 @@ resource "helm_release" "kong-public" {
 }
 
 resource "helm_release" "kong_echo_service_gateway_api" {
-  count   = (var.gateway_flavour == "kong" && var.gateway_api == true) ? 1 : 0
-  name    = "kong-echo-service-gateway-api"
-  chart   = "./gateway/kong-echo-service-gateway-api"
-  namespace = "echo-service"
+  count            = (var.gateway_flavour == "kong" && var.gateway_api == true) ? 1 : 0
+  name             = "kong-echo-service-gateway-api"
+  chart            = "./gateway/kong-echo-service-gateway-api"
+  namespace        = "echo-service"
   create_namespace = true
-  timeout = 600
-  depends_on = [helm_release.kong-public]
+  timeout          = 600
+  depends_on       = [helm_release.kong-public]
 }
 
 resource "helm_release" "kong_echo_service_ingress" {
-  count   = (var.gateway_flavour == "kong" && var.gateway_api == false) ? 1 : 0
-  name    = "kong-echo-service-ingress"
-  chart   = "./gateway/kong-echo-service-ingress"
-  namespace = "echo-service"
+  count            = (var.gateway_flavour == "kong" && var.gateway_api == false) ? 1 : 0
+  name             = "kong-echo-service-ingress"
+  chart            = "./gateway/kong-echo-service-ingress"
+  namespace        = "echo-service"
   create_namespace = true
-  timeout = 600
-  depends_on = [helm_release.kong-public]
+  timeout          = 600
+  depends_on       = [helm_release.kong-public]
 }
 
 resource "helm_release" "nginx_echo_service_gateway_api" {
-  count   = (var.gateway_flavour == "nginx")  ? 1 : 0
-  name    ="nginx-echo-service-gateway-api"
-  chart   = "./gateway/nginx-echo-service-gateway-api"
-  namespace = "echo-service"
+  count            = (var.gateway_flavour == "nginx") ? 1 : 0
+  name             = "nginx-echo-service-gateway-api"
+  chart            = "./gateway/nginx-echo-service-gateway-api"
+  namespace        = "echo-service"
   create_namespace = true
-  timeout = 600
-  depends_on = [helm_release.nginx_gateway_fabric]
+  timeout          = 600
+  depends_on       = [helm_release.nginx_gateway_fabric]
 }
